f7351cafbe17783ea5ce6a7a65696a8a3a65454b
Boan Smith <boansmith@gmail.com>
2015-07-19 16:45:09 +0800

    Create globalparams.cpp

---
+++ globalparams.cpp
@@ -0,0 +1,11 @@
+#include "globalparams.h"
+
+const float GlobalParams::m_fPermConnected      = 0.2;
+const int   GlobalParams::m_nActiveThreshHold   = 80;
+const int   GlobalParams::m_nCellsPerCol        = 4;
+const int   GlobalParams::m_nWidthInput         = 20;
+const int   GlobalParams::m_nHeightInput        = 20;
+const float GlobalParams::m_fDeltaPermInc       = 0.01;
+const float GlobalParams::m_fDeltaPermDec       = 0.01;
+const int   GlobalParams::m_nWidthColumn        = 10;
+const int   GlobalParams::m_nHeightColumn       = 10;
